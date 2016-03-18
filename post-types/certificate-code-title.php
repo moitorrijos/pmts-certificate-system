39954cc57c2d660aea787ffb886c9c3b6dbe0c79
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2016-03-18 17:35:41 -0500

    Add missing accounting fields in certificates. Fix participant's name field in certificates.

--- post-types/certificate-code-title.php
+++ post-types/certificate-code-title.php
@@ -5,26 +5,17 @@ add_filter( 'wp_insert_post_data' , 'modify_post_title' , '99', 2 );
 function modify_post_title( $data , $postarr )
 {
 
-    if( $data['post_type'] == 'quotation' ) {
+    if( $data['post_type'] == 'certificates' ) {
     	 $args = array(
-            'numberposts' => 1,
-            'orderby' => 'post_date',
-            'order' => 'DESC',
-            'post_type' => 'quotation',
-            'post_status' => 'publish'
-        );
-        $last_post = wp_get_recent_posts($args);
-        $last__post_id = (int)$last_post[0]['ID'];
-        $data['post_title'] = 'PMTS/Q/' . date('Y') . '/' . ($last__post_id + 1);
-    } else if ( $data['post_type'] == 'certificates' ) {
-	    	 $args = array(
             'numberposts' => 1,
             'orderby' => 'post_date',
             'order' => 'DESC',
             'post_type' => 'certificates',
             'post_status' => 'publish'
         );
-        $data['post_title'] = 'Certificate/' . date('y-m-d-H-i-s') ;
+        $last_post = wp_get_recent_posts($args);
+        $last__post_id = (int)$last_post[0]['ID'];
+        $data['post_title'] = 'PMTS/Q/' . date('Y') . '/' . ($last__post_id + 1);
     }
     return $data;
 }
