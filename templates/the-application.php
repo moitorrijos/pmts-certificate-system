93278395370ddd851869be3ee8ba5917b5827e36
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2018-03-04 15:55:02 -0500

    Styling fixes.

--- templates/the-application.php
+++ templates/the-application.php
@@ -5,41 +5,28 @@
 		<div class="buttons">
 			
 			<a href="<?php echo home_url( 'application-forms' ); ?>" class="back-link">
-				
 				&laquo;
-
 				Back to Application Forms
-
 			</a>
 
 			<a href="#0" class="edit-button not-link"><i class="fa fa-pencil"></i>
-
-					Edit Application
-
-				</a>
+				Edit Application
+			</a>
 
 			<a href="#0" class="print-button not-link"><i class="fa fa-print"></i>
-
 				Print Application
-
 			</a>
 
 			<a href="#0" class="view-button not-link"><i class="fa fa-eye"></i>
-
 				View Application
-
 			</a>
 
 			<a href="#0" class="duplicate-certificate-button" id="duplicate-application"><i class="fa fa-clone"></i>
-				
 				Duplicate Application
-
 			</a>
 
 			<a href="<?php echo get_permalink(7190);?>" class="new-certificate-button"><i class="fa fa-plus-square"></i>
-
 				Create New Application
-				
 			</a>
 
 		</div>
@@ -49,21 +36,13 @@
 			if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
 			$participants_name = get_field('participants_name_app');
-
 			$participants_id = get_field('passport_id_app');
-
 			$participants_place_of_birth = get_field('place_of_birth_app');
-
 			$participants_nationality = get_field('nationality_app');
-
 			$participants_rank = get_field('rank_app');
-
 			$participants_date_of_birth = DateTime::createFromFormat( 'Ymd', get_field('date_of_birth_app') );
-
 			$place_of_training_slug = get_field('place_of_training_app');
-
 			$place_of_training = get_term_by( 'slug', $place_of_training_slug, 'office' );
-
 			$delivery_mode = get_field('course_deliver_mode_app');
 
 		?>
@@ -73,22 +52,18 @@
 			<div class="full application-sent">
 
 				<p>
-
 					The certificate has been sent to PMTS Main Office for printing. 
 					If you need any changes please contact the main office. The local time in Panama is 
 					<?php  
 						date_default_timezone_set("America/Panama");
 						echo date("h:i a");
 					?>
-
 				</p>
 
 			</div>
 
 			<div class="full application-sent-error">
-				
 				<p>
-					
 					Sorry, there was a problem, please try again later. 
 					If  you need to send this application please send it via email to 
 					<a href="mailto:certificates@panamamaritimetraining.com">
@@ -109,101 +84,61 @@
 
 
 				<div class="thirds">
-					
 					<p>
 						Full Name: 
-
 						<span class="undies">
-
 							<?php echo $participants_name; ?>
-							
 						</span>
-
 					</p>
-
-					
 				</div>
 
 				<div class="thirds">
-					
 					<p>
-
 						Passport No. or National ID:
-
 						<span class="undies">
-						
 							<?php echo $participants_id; ?>
-
 						</span>
-
 					</p>
 
 				</div>
 
 				<div class="thirds">
-					
 					<p>
-						
 						Date of Birth:
-
 						<span class="undies">
-							
 							<?php echo $participants_date_of_birth->format('d F Y'); ?>
-
 						</span>
-
 					</p>
-
 				</div>
 				
 			</div>
 
 			<div class="full">
-				
 				<div class="thirds">
-					
 					<p>
-						
 						Place of Birth:
-
 						<span class="undies">
-
 							<?php echo $participants_place_of_birth; ?>
-
 						</span>
-
 					</p>
-
 				</div>
 
 				<div class="thirds">
-					
 					<p>
-						
 						Nationality:
-
 						<span class="undies">
-							
 							<?php echo $participants_nationality; ?>
-
 						</span>
-
 					</p>
 
 				</div>
 
 				<div class="thirds">
-					
 					<p>
-						
 						Place of Training
-
 						<span class="undies">
-							
 							<?php echo $place_of_training->name; ?>
-
 						</span>
-
 					</p>
 
 				</div>
@@ -226,11 +161,9 @@
 								<th class="short-title">End Date</th>
 								<th class="short-number">Time</th>
 								<th class="short-number">Students</th>
-								<?php // if ( current_user_can('moderate_comments') ) : ?>
 								<th class="short-number">
-										Print
-									</th>
-								<?php // endif; ?>
+									Print
+								</th>
 							</tr>
 						</thead>
 
@@ -261,17 +194,11 @@
 									data-instructor_id="<?php if ( $instructor ) echo $instructor->ID; ?>"
 								>
 									<?php 
-
 										if( $instructor ) {
-
 											echo $instructor->post_title;
-
 										} else {
-
 											echo '<span class="blackies">TBA</span>' ;
-
 										}
-
 									?>
 								</td>
 								<td 
@@ -279,15 +206,10 @@
 									data-start_date="<?php if ($start_date) echo $start_date->format('Ymd'); ?>"
 								>
 									<?php 
-
 										if( $start_date ) { 
-
 											echo $start_date->format('d F Y');
-
 										} else {
-
 											echo '<span class="blackies">TBA</span>' ;
-
 										}
 									?>
 								</td>
@@ -296,15 +218,10 @@
 									data-end_date="<?php if($end_date) echo $end_date->format('Ymd'); ?>"
 								>
 									<?php 
-
 										if( $end_date ) { 
-
 											echo $end_date->format('d F Y');
-
 										} else {
-
 											echo '<span class="blackies">TBA</span>' ;
-
 										}
 									?>
 								</td>
@@ -325,30 +242,29 @@
 									?>
 								</td>
 								<td class="centered printly">
-											<?php
-												$certificate_exists = certificate_exists($participants_id, $course);
-												if (!$certificate_exists) : 
-											?>
-												<?php if ( current_user_can( 'moderate_comments' ) && $instructor && $start_date && $end_date ) : ?>
-													<a href="#0" class="create-certificate-button">
-														<i class="fa fa-print"></i>
-													</a>
-												<?php else : ?>
-													<p href="#0" class="warningly">
-														<i class="fa fa-times" aria-hidden="true"></i>
-													</p>
-												<?php endif; ?>
-											<?php else : ?>
-												<a 
-													class="not-link"
-													href="<?php echo $certificate_exists[0]->guid; ?>"
-													target="_blank"
-												>
-													<i class="fa fa-external-link"></i>
-												</a>
-											<?php endif; ?>
+									<?php
+										$certificate_exists = certificate_exists($participants_id, $course);
+										if (!$certificate_exists) : 
+									?>
+										<?php if ( current_user_can( 'moderate_comments' ) && $instructor && $start_date && $end_date ) : ?>
+											<a href="#0" class="create-certificate-button">
+												<i class="fa fa-print"></i>
+											</a>
+										<?php else : ?>
+											<p href="#0" class="warningly">
+												<i class="fa fa-times" aria-hidden="true"></i>
+											</p>
+										<?php endif; ?>
+									<?php else : ?>
+										<a 
+											class="not-link"
+											href="<?php echo $certificate_exists[0]->guid; ?>"
+											target="_blank"
+										>
+											<i class="fa fa-external-link"></i>
 										</a>
-									</td>
+									<?php endif; ?>
+								</td>
 							</tr>
 
 						<?php endwhile; ?>
