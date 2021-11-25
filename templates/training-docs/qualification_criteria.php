8d7503fc4ee481ac6557f5f62ffaec534c172c45
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2021-11-25 11:36:47 -0500

    Changes styles.

---
+++ templates/training-docs/qualification_criteria.php
@@ -0,0 +1,33 @@
+<div class="required-documents">
+
+  <h3>QUALIFICATION CRITERIA</h3>
+
+  <ol>
+    <li>The <strong>minimum percentage</strong> of approval for the Practical Test is <strong>71%</strong></li>
+    <li>For the case of Theoretical and Practical Course, the Rating Weight Percentage to evaluate is: <strong>Theoretical 40% and Practical 60%</strong>. </li>
+    <li>Performance Standard shall be graded <strong>DONE</strong> or <strong>NOT DONE</strong>, for each criteria.</li>
+    <li>In the case the candidate complete the task out of the time duration will be mark as <strong>NOT DONE</strong> and should complete again the tasks, within the time duration established for each task.</li>
+    <li>In case the candidate failure to do the task and out of the time duration, will be mark as <strong>NOT DONE</strong></li>
+    <li>A candidate who fails in the same task (3) times, shall not be allowed to re-take the exam, until the complete again the training course.</li>
+    <li>In the case the candidate obtain less than 71pts, should complete again the training course and repeat the practical exam, to comply with the performance criteria</li>
+    <li>Assessment Final Criteria shall be graded <strong>COMPETENT</strong> or <strong>NOT COMPETENT</strong></li>
+  </ol>
+
+</div>
+
+<h4 class="center-align">REPORT OF PRACTICAL EVALUATION CRITERIA FOR SEAFARERS PROFICIENCY &#10003;</h4>
+
+<table class="evaluation-score passed-failed">
+		<thead>
+			<tr>
+				<th>Competent</th>
+				<th>Not Competent</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
