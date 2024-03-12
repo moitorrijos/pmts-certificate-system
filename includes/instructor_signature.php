156752408dfe07d2748f8cdde778a737145e4307
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2024-03-12 15:28:27 -0500

    Refactor instructor_signature function to handle missing instructor signature image

--- includes/instructor_signature.php
+++ includes/instructor_signature.php
@@ -1,16 +1,14 @@
 <?php
 
 function instructor_signature( $instructor ) {
-
 	$instructor_signature = get_field('instructor_digital_signature', $instructor->ID);
-
 	?>
-		<div 
-			class="general-director" 
+		<div class="general-director">
 			<?php if ( $instructor_signature ) : ?>
-				style="background: url(<?php echo $instructor_signature; ?>) no-repeat; background-size: 240px 120px; background-position: center top;"
+				<img class="digital_signature" style="left: <?php echo rand(-10, 10); ?>px; bottom: <?php echo rand(25, 35);?>px; width: <?php echo rand(250, 350); ?>px" src="<?php echo $instructor_signature; ?>" alt="">
+			<?php else : ?>
+				<img class="digital_signature" style="left: <?php echo rand(-10, 10); ?>px; bottom: <?php echo rand(25, 35);?>px; width: <?php echo rand(200, 300); ?>px" src="<?php echo IMAGESPATH . "/firma-agustin-gonzalez66.svg"; ?>" alt="">
 			<?php endif; ?>
-		>
 			<div class="signature-line"></div>
 			<p class="super-short">
 			Academic Director's / Instructor's Signature
